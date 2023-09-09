447500d5e1eba52cea59a3224e674e8739373f6d
msmsm104 <msmsm104@naver.com>
2023-09-09 15:39:14 +0900

    select_fruit.sh 수행하는 DAG 시간 오타 수정

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asiz/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
