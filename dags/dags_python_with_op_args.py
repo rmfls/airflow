a9de491361a69cdc4ddfe1155b37abcc97836775
msmsm104 <msmsm104@naver.com>
2023-09-11 20:51:44 +0900

    op_args 테스트용 dag 업로드

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['msmsm104', 'man', 'kr', 'seoul']
+    )
+
+    regist_t1
+
+    # @task(task_id='regist_t1')
+    # regist()
