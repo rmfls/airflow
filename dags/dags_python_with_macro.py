fad74cc87eec4724b89e927686373821ed4ac6e2
msmsm104 <msmsm104@naver.com>
2023-09-12 20:38:23 +0900

    python macro DAG 업로드_2

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -25,5 +25,15 @@ with DAG(
             end_date = templates_dict.get('end_date') or 'end_date없음'
             print(start_date)
             print(end_date)
-    
-    get_datetime_macro()
+
+    @task(task_id = 'task_direct_calc')
+    def get_datetime_calc(**kwargs):
+        from dateutil.relativedelta import relativedelta
+
+        data_interval_end = kwargs['data_interval_end']
+        prev_month_day_first = data_interval_end.in_timezone("Asia/Seoul") + relativedelta(months=-1, day=1)
+        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + relativedelta(days=-1)
+        print(prev_month_day_first.strftime('%Y-%m-%d'))
+        print(prev_month_day_last.strftime('%Y-%m-%d'))
+
+    get_datetime_macro() >> get_datetime_calc()
