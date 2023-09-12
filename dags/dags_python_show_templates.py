b5b60db74fb61c4a9c8022f4efe1b4e9179e202c
msmsm104 <msmsm104@naver.com>
2023-09-12 16:50:22 +0900

    show templates DAG 재업로드

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -10,7 +10,7 @@ with DAG(
     catchup=True
 ) as dag:
     
-    @task(task_id = "python_task")
+    @task(task_id="python_task")
     def show_templates(**kwargs):
         from pprint import pprint
         pprint(kwargs)
