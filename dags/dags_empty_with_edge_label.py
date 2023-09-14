e7b99b1017ccc008cbc3e2880ee189820dbf9d06
msmsm104 <msmsm104@naver.com>
2023-09-14 14:00:44 +0900

    work flow 수정

--- dags/dags_empty_with_edge_label.py
+++ dags/dags_empty_with_edge_label.py
@@ -18,6 +18,8 @@ with DAG(
         task_id = 'empty_2'
     )
 
+    empty_1 >> Label('1과 2사이') >> empty_2
+
     empty_3 = EmptyOperator(
         task_id = 'empty_3'
     )
@@ -34,4 +36,4 @@ with DAG(
         task_id = 'empty_6'
     )
 
-    empty_1 >> Label('') >> [empty_2, empty_3, empty_4] >> Label('') >> empty_6
+    empty_2 >> Label('Start Branch') >> [empty_3, empty_4, empty_5] >> Label('End Branch') >> empty_6
