1cfa1d4a53819895eb76ccecb1213af2aece14b3
msmsm104 <msmsm104@naver.com>
2023-09-14 13:58:10 +0900

    코드 추가

---
+++ dags/dags_empty_with_edge_label.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.edgemodifier import Label
+
+with DAG(
+    dag_id='dags_empty_with_edge_label',
+    start_date=pendulum.datetime(2023, 9, 10, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    empty_1 = EmptyOperator(
+        task_id = 'empty_1'
+    )
+
+    empty_2 = EmptyOperator(
+        task_id = 'empty_2'
+    )
+
+    empty_3 = EmptyOperator(
+        task_id = 'empty_3'
+    )
+
+    empty_4 = EmptyOperator(
+        task_id = 'empty_4'
+    )
+
+    empty_5 = EmptyOperator(
+        task_id = 'empty_5'
+    )
+
+    empty_6 = EmptyOperator(
+        task_id = 'empty_6'
+    )
+
+    empty_1 >> Label('') >> [empty_2, empty_3, empty_4] >> Label('') >> empty_6
