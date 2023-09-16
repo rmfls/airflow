1cd07b7945adc04091c940a6d4bf0dcc16f09be6
msmsm104 <msmsm104@naver.com>
2023-09-16 17:31:33 +0900

    코드 수정

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -50,7 +50,7 @@ class SeoulApiToCsvOperator(BaseOperator):
         request_url = f'{base_url}/{start_row}/{end_row}'
         if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
-        response = requests.get(request_url, headers)
+        response = requests.get(request_url, headers=headers)
         contests = json.loads(response.text)
 
         key_nm = list(contests.keys())[0]
