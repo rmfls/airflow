e6cfa9db7c5bbfda56e449463e022725aad50762
msmsm104 <msmsm104@naver.com>
2023-09-30 18:32:10 +0900

    google_sheet_conn TEST

--- dags/dags_google_sheet_test.py
+++ dags/dags_google_sheet_test.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import gspread
-from oauth2client import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 default_args = {
     'owner': 'airflow',
@@ -14,12 +14,19 @@ default_args = {
 
 def test_google_sheets_conn():
     # Google Sheets API와 연동하기 위한 인증
-    scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('/path_to_your_service_account_key.json', scope)
+    # OAuth2 인증을 위한 설정
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/spreadsheets"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("api_json/galvanic-ripsaw-399809-a3237f94c91e.json", scope)
     client = gspread.authorize(creds)
+    sheet = client.open("KN 광고 관리 문서")
+
     
     # 특정 시트 읽기 (예: 'Sheet1'라는 이름의 시트)
-    sheet = client.open('Your_Spreadsheet_Name').sheet1
+    # sheet = client.open('Your_Spreadsheet_Name').sheet1
     values = sheet.get_all_values()
     for row in values:
         print(row)
