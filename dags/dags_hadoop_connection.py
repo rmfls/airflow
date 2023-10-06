a145ce3235d81dbf9df9b145b23372aeaf769f93
msmsm104 <msmsm104@naver.com>
2023-10-06 18:40:21 +0900

    주석에 수정 내용 추가

--- dags/dags_hadoop_connection.py
+++ dags/dags_hadoop_connection.py
@@ -9,6 +9,8 @@ import os
 
 '''
 경로 설정을 실제 공용하둡에서 사용하는것과 동일한 형태로 가져가고 싶다.
+하둡 경로 : 베이스 경로 - 유저 이름 - 프로젝트 이름 
+구글시트 경로 (로컬 경로) : 베이스 경로 - 프로젝트 이름 
 '''
 
 
@@ -19,11 +21,13 @@ with DAG(
     schedule=None,
     catchup=False
 ) as dag:
-    BASE_PATH = Variable.get('hadoop_base_path')
+    BASE_PATH = Variable.get('hadoop_base_path') # airflow UI에 등록한 값 => /tmp3/kidsnote/kidsnote
     PROJECT_NAME = "gcp"
     LOCAL_PATH = f"/Users/green/airflow/files/{PROJECT_NAME}/"
     USER = "green"
 
+    # 하둡에 유저의 경로가 존재하지 않으면 만들어야 함.
+    # 위에서 설정한 변수들을 simplehttp operator에 필수 입력 값으로 받아야함.
     hdfs_put_cmd = SimpleHttpOperator(
         task_id='hdfs_put_cmd',
         method='POST',
@@ -32,9 +36,9 @@ with DAG(
         data=json.dumps({
             'option': 'put',
             'hadoop_base_path': BASE_PATH,
-            'project_name': PROJECT_NAME, # 변수로 설정
+            'project_name': PROJECT_NAME, 
             'local_path': LOCAL_PATH,
-            'user': USER # 스크립트가 돌아가는 상대경로 (변수 설정)
+            'user': USER 
         }),
         headers={'Content-Type': 'application/json'}
     )
