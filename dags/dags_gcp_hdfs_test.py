a66fe8917b3b5f26a97a15ae83cde22f6a4b77b7
msmsm104 <msmsm104@naver.com>
2023-10-08 17:37:43 +0900

    수정

--- dags/dags_gcp_hdfs_test.py
+++ dags/dags_gcp_hdfs_test.py
@@ -13,9 +13,10 @@ default_args = {
     'project_nm': 'gcp'
 }
 
-def download_google_sheet(task_instance, project_nm):
+def download_google_sheet(**kwargs):
+    project_nm = kwargs['project_nm']
     hook = GoogleSheetsHook(gcp_conn_id='sheet_conn_id_test', project_nm=project_nm)
-    hook.save_sheets_as_parquet(spreadsheet_name='KN 광고 관리 문서', task_instance=task_instance)
+    hook.save_sheets_as_parquet(spreadsheet_name='KN 광고 관리 문서', task_instance=kwargs['ti'])
 
 def extract_schema_from_parquet(**context):
     local_path = f"/Users/green/airflow/files/gcp/"
@@ -39,8 +40,8 @@ with DAG(
     read_sheet_task = PythonOperator(
         task_id='read_sheet_task',
         python_callable=download_google_sheet,
-        op_args=[default_args['project_nm']],
-        provide_context=True
+        provide_context=True,
+        dag=dag
     )
 
     hdfs_put_cmd = SimpleHttpOperator(
