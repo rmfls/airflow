2cc8ac1b0b6df200feac48f0918417b8410494b1
msmsm104 <msmsm104@naver.com>
2023-10-08 16:37:56 +0900

    내용 추가

---
+++ dags/dags_gcp_hdfs_test.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.http.operators.http import SimpleHttpOperator
+import json
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pendulum
+from operators.load_google_sheet import GoogleSheetsHook
+
+def download_google_sheet(project_nm):
+    hook = GoogleSheetsHook(gcp_conn_id='sheet_conn_id_test', project_nm=project_nm)
+    hook.save_sheets_as_parquet(spreadsheet_name='KN 광고 관리 문서')
+
+default_args = {
+    'project_nm': 'gcp'
+}
+
+with DAG(
+    dag_id='dags_gcp_hdfs_test',
+    description='Read data from Google Sheets & put to Hadoop',
+    start_date=pendulum.datetime(2023, 10, 1, tz='Asia/Seoul'),
+    catchup=False,
+    default_args=default_args
+) as dag:
+    
+    read_sheet_task = PythonOperator(
+        task_id='read_sheet_task',
+        python_callable=download_google_sheet,
+        op_args=[default_args['project_nm']]
+    )
+
+    hdfs_put_cmd = SimpleHttpOperator(
+        task_id='hdfs_put_cmd',
+        method='POST',
+        endpoint='/hdfs_cmd',
+        http_conn_id='local_fast_api_conn_id',
+        data=json.dumps({
+            'option': 'put',
+            'hadoop_base_path': Variable.get('hadoop_base_path'),
+            'project_name': 'gcp',
+            'local_path': f"/Users/green/airflow/files/gcp/",
+            'user': 'green' 
+        }),
+        headers={'Content-Type': 'application/json'}
+    )
+
+    read_sheet_task >> hdfs_put_cmd
+
