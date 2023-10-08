051e98e3cb5fc23234ec214620246a52518e169e
msmsm104 <msmsm104@naver.com>
2023-10-08 17:15:34 +0900

    hive schema, table 생성 task 추가

--- dags/dags_gcp_hdfs_test.py
+++ dags/dags_gcp_hdfs_test.py
@@ -6,14 +6,27 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import pendulum
 from operators.load_google_sheet import GoogleSheetsHook
+from operators.hive_schema import generate_hive_schema_from_parquet
+from pathlib import Path
+
+default_args = {
+    'project_nm': 'gcp'
+}
 
 def download_google_sheet(project_nm):
     hook = GoogleSheetsHook(gcp_conn_id='sheet_conn_id_test', project_nm=project_nm)
     hook.save_sheets_as_parquet(spreadsheet_name='KN 광고 관리 문서')
 
-default_args = {
-    'project_nm': 'gcp'
-}
+def extract_schema_from_parquet(**context):
+    local_path = f"/Users/green/airflow/files/gcp/"
+    parquet_files = [f for f in Path(local_path).glob('*.parquet')]
+
+    if not parquet_files:
+        raise Exception('No parquet files found!')
+    
+    schema = generate_hive_schema_from_parquet(parquet_files[0])
+    context['ti'].xcom_push(key='hive_schema', value=schema)
+
 
 with DAG(
     dag_id='dags_gcp_hdfs_test',
@@ -44,5 +57,27 @@ with DAG(
         headers={'Content-Type': 'application/json'}
     )
 
-    read_sheet_task >> hdfs_put_cmd
+    # extract schema from parquet
+    extract_schema_task = PythonOperator(
+        task_id='extract_schema_task',
+        python_callable=extract_schema_from_parquet,
+        provide_context=True
+    )
 
+    create_hive_table_cmd = SimpleHttpOperator(
+        task_id='create_hive_table_cmd',
+        method='POST',
+        endpoint='/hive_cmd',
+        http_conn_id='local_fast_api_conn_id',
+        data=json.dumps({
+            'option': 'create',
+            'database_name': 'gcp',
+            'table_name': '',
+            'schema': "{{ ti.xcom_pull(task_ids='extract_schema_from_parquet', key='hive_schema')}}",
+            'project_name': 'gcp'
+        }),
+        headers={'Content-Type': 'application/json'}
+    )
+
+
+    read_sheet_task >> hdfs_put_cmd >> extract_schema_task >> create_hive_table_cmd
---
+++ plugins/operators/hive_schema.py
@@ -0,0 +1,42 @@
+import pyarrow.parquet as pq
+
+def generate_hive_schema_from_parquet(parquet_path):
+    # Parquet 파일의 스키마 읽기
+    table = pq.read_table(parquet_path)
+    schema = table.schema
+
+    type_mapping = {
+        'int8': 'TINYINT',
+        'int16': 'SMALLINT',
+        'int32': 'INT',
+        'int64': 'BIGINT',
+        'uint8': 'INT',  # Hive does not support unsigned integers, so casting them.
+        'uint16': 'INT',
+        'uint32': 'BIGINT',
+        'uint64': 'BIGINT',  # Beware: Overflow might happen.
+        'float16': 'FLOAT',
+        'float32': 'FLOAT',
+        'float64': 'DOUBLE',
+        'bool': 'BOOLEAN',
+        'string': 'STRING',
+        'binary': 'BINARY',
+        'timestamp[ns]': 'TIMESTAMP',
+        'timestamp[us]': 'TIMESTAMP',
+        'date32[day]': 'DATE',
+        'double': 'DOUBLE',
+        'fixed_size_binary': 'BINARY',  # you might need more logic based on the size.
+        # Additional types can be added as needed
+    }
+
+    columns = []
+    for field in schema:
+        field_type = str(field.type)
+
+        # 해당 컬럼의 데이터 유형이 알려진 유형인 경우 매핑하여 칼럼 유형 추가
+        if field_type in type_mapping:
+            columns.append(f"{field.name} {type_mapping[field_type]}")
+        else:
+            # 알려지지 않은 타입에 대한 처리
+            columns.append(f"{field.name} STRING")
+
+    return ', '.join(columns)
