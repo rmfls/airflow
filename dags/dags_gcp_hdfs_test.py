ddf87fbd6192a1514c060e84529f6b02f1ad87b2
msmsm104 <msmsm104@naver.com>
2023-10-09 17:23:34 +0900

    주석처리

--- dags/dags_gcp_hdfs_test.py
+++ dags/dags_gcp_hdfs_test.py
@@ -18,15 +18,15 @@ def download_google_sheet(**kwargs):
     hook = GoogleSheetsHook(gcp_conn_id='sheet_conn_id_test', project_nm=project_nm)
     hook.save_sheets_as_parquet(spreadsheet_name='KN 광고 관리 문서', task_instance=kwargs['ti'])
 
-def extract_schema_from_parquet(**context):
-    local_path = f"/Users/green/airflow/files/gcp/"
-    parquet_files = [f for f in Path(local_path).glob('*.parquet')]
+# def extract_schema_from_parquet(**context):
+#     local_path = f"/Users/green/airflow/files/gcp/"
+#     parquet_files = [f for f in Path(local_path).glob('*.parquet')]
 
-    if not parquet_files:
-        raise Exception('No parquet files found!')
+#     if not parquet_files:
+#         raise Exception('No parquet files found!')
     
-    schema = generate_hive_schema_from_parquet(parquet_files[0])
-    context['ti'].xcom_push(key='hive_schema', value=schema)
+#     schema = generate_hive_schema_from_parquet(parquet_files[0])
+#     context['ti'].xcom_push(key='hive_schema', value=schema)
 
 
 with DAG(
@@ -59,27 +59,27 @@ with DAG(
         headers={'Content-Type': 'application/json'}
     )
 
-    # extract schema from parquet
-    extract_schema_task = PythonOperator(
-        task_id='extract_schema_task',
-        python_callable=extract_schema_from_parquet,
-        provide_context=True
-    )
+    # # extract schema from parquet
+    # extract_schema_task = PythonOperator(
+    #     task_id='extract_schema_task',
+    #     python_callable=extract_schema_from_parquet,
+    #     provide_context=True
+    # )
 
-    create_hive_table_cmd = SimpleHttpOperator(
-        task_id='create_hive_table_cmd',
-        method='POST',
-        endpoint='/hive_cmd',
-        http_conn_id='local_fast_api_conn_id',
-        data=json.dumps({
-            'option': 'create',
-            'database_name': 'gcp',
-            'table_name': '',
-            'schema': "{{ ti.xcom_pull(task_ids='extract_schema_from_parquet', key='hive_schema')}}",
-            'project_name': 'gcp'
-        }),
-        headers={'Content-Type': 'application/json'}
-    )
+    # create_hive_table_cmd = SimpleHttpOperator(
+    #     task_id='create_hive_table_cmd',
+    #     method='POST',
+    #     endpoint='/hive_cmd',
+    #     http_conn_id='local_fast_api_conn_id',
+    #     data=json.dumps({
+    #         'option': 'create',
+    #         'database_name': 'gcp',
+    #         'table_name': '',
+    #         'schema': "{{ ti.xcom_pull(task_ids='extract_schema_from_parquet', key='hive_schema')}}",
+    #         'project_name': 'gcp'
+    #     }),
+    #     headers={'Content-Type': 'application/json'}
+    # )
 
 
-    read_sheet_task >> hdfs_put_cmd >> extract_schema_task >> create_hive_table_cmd
+    read_sheet_task >> hdfs_put_cmd 
