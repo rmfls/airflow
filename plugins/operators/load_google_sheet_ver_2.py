77904fb94b2e253cdb0bbe1c076fbe36377cff5c
msmsm104 <msmsm104@naver.com>
2023-10-19 18:18:50 +0900

    컬럼 매핑 값 추가

--- plugins/operators/load_google_sheet_ver_2.py
+++ plugins/operators/load_google_sheet_ver_2.py
@@ -82,7 +82,8 @@ class GoogleSheetsHook(GoogleBaseHook):
     COLUMN_NAME_MAPPING_04 = {
         "날짜": "push_date",
         "Log 등록 여부": "Log_Registration_Status",
-        "Target url": "target_url"
+        "Target url": "target_url",
+        "#push": "push_price"
     }
 
     def __init__(self, gcp_conn_id='google_cloud_default', project_nm='', *args, **kwargs):
