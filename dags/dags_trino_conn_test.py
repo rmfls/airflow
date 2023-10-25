ca32072112b45548095eb3c842f0f2506dab05c9
msmsm104 <msmsm104@naver.com>
2023-10-25 17:36:43 +0900

    fastapi 로직 추가

--- dags/dags_trino_conn_test.py
+++ dags/dags_trino_conn_test.py
@@ -1,47 +1,9 @@
 from airflow.hooks.base import BaseHook
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+from airflow.providers.http.operators.http import SimpleHttpOperator
 import pendulum
-
-
-
-def fetch_data_from_trino():
-    from trino.dbapi import connect
-    from trino.auth import BasicAuthentication
-    import pandas as pd
-
-    # Airflow Connection 에서 정보 가져오기
-    conn_info = BaseHook.get_connection('trino_conn')
-
-    conn = connect(
-        host=conn_info.host,
-        port=conn_info.port,
-        user=conn_info.login,
-        auth=BasicAuthentication(conn_info.login, conn_info.password),
-        http_scheme='https',
-        catalog='hadoop_doopey',
-        schema=conn_info.schema
-    )
-
-    cur = conn.cursor()
-    
-    query = '''
-        select
-        id,
-        date (created + interval '9' hour) as ymd,
-        content,
-        category_id
-        from
-        kn_users_note
-    '''
-    
-    cur.execute(query)
-    store_gg = cur.fetchall()
-    df = pd.DataFrame(store_gg, columns=['id', 'date', 'content', 'category_id'])
-
-    print(df)
-
+import json
 
 
 with DAG(
@@ -51,13 +13,30 @@ with DAG(
     catchup=False
 ) as dag:
     
-    fetch_data_task = PythonOperator(
-        task_id='fetch_data_task',
-        python_callable=fetch_data_from_trino,
+    # Airflow connection 정보 가져오기
+    conn_info = BaseHook.get_connection('trino_conn')
+
+    load_from_trino_task = SimpleHttpOperator(
+        task_id='load_from_trino_task',
+        method='POST',
+        http_conn_id='fastapi_conn',
+        endpoint='/trino_query',
+        data=json.dumps({
+            'query': 'select * from schema.table_name',
+            'connection': {
+                'host': conn_info.host,
+                'port': conn_info.port,
+                'user': conn_info.login,
+                'password': conn_info.password,
+                'catalog': 'hadoop_doopey',
+                'schema': 'kidsnote'
+            }
+        }),
+        headers={'Content-Type': 'application/json'},
         dag=dag
     )
 
-    fetch_data_task
-
+    load_from_trino_task
+    
 
     
