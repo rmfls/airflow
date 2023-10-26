1987309bc908e25f706dda6b712620005b12dbe5
msmsm104 <msmsm104@naver.com>
2023-10-26 15:17:15 +0900

    함수 모듈화

--- dags/dags_trino_conn_test_2.py
+++ dags/dags_trino_conn_test_2.py
@@ -3,41 +3,22 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import pendulum
 
+from operators.fetch_data_from_trino import fetch_data_from_trino
 
-def fetch_data_from_trino():
-    from trino.dbapi import connect
-    from trino.auth import BasicAuthentication
-    import pandas as pd
 
-    # Airflow Connection에서 정보 가져오기
-    conn_info = BaseHook.get_connection('trino_conn')
-
-    conn = connect(
-        host=conn_info.host,
-        port=conn_info.port,
-        user=conn_info.login,
-        auth=BasicAuthentication(conn_info.login, conn_info.password),
-        http_scheme="https",  # 이 부분은 HTTP 연결 타입에 따라 조정해야 할 수 있습니다.
-        catalog="hadoop_doopey",
-        schema=conn_info.schema,
-    )
-
-    cur = conn.cursor()
-    
-    query = '''
+# 쿼리
+query = '''
     select
-      id,
-      date (created + interval '9' hour) as ymd,
-      content,
-      category_id
-    from
-      kn_users_note
-    '''
-    
-    cur.execute(query)
-    store_gg = cur.fetchall()
-    df = pd.DataFrame(store_gg, columns=['id', 'date', 'content', 'category_id'])
-    print(df)
+        id,
+        date (created + interval '9' hour) as ymd,
+        content,
+        category_id
+    from 
+        kn_users_note
+'''
+
+columns = ['id', 'ymd', 'content', 'category_id']
+
 
 with DAG(
     dag_id='dags_trino_conn_test_2',
@@ -49,6 +30,7 @@ with DAG(
     fetch_data_task = PythonOperator(
         task_id='fetch_data_from_trino',
         python_callable=fetch_data_from_trino,
+        op_kwargs={'query': query, 'columns': columns},
         dag=dag,
     )
 
---
+++ plugins/operators/fetch_data_from_trino.py
@@ -0,0 +1,24 @@
+from trino.dbapi import connect
+from trino.auth import BasicAuthentication
+from airflow.hooks.base import BaseHook
+import pandas as pd
+
+def fetch_data_from_trino(query, columns):
+    # Airflow Connection에서 정보 가져오기
+    conn_info = BaseHook.get_connection('trino_conn')
+
+    conn = connect(
+        host=conn_info.host,
+        port=conn_info.port,
+        user=conn_info.login,
+        auth=BasicAuthentication(conn_info.login, conn_info.password),
+        http_scheme='https',
+        catalog='hadoop_doopey',
+        schema=conn_info.schema
+    )
+
+    cur = conn.cursor()
+    cur.execute(query)
+    store_gg = cur.fetchall()
+    df = pd.DataFrame(store_gg, columns=columns)
+    print(df)
